c7eb93c6d5006dc0cd7fc1adec7b86a0566ea397
yuyoux7 <91305304+yuyoux7@users.noreply.github.com>
2023-10-17 21:16:16 +0800

    Update main.cpp

--- test/class_02/main.cpp
+++ test/class_02/main.cpp
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+int main()
+{
+  char ch;
+  printf("input / or '");
+  scanf("%c", &ch);
+  if(ch == '/')
+    printf("/");
+  else if(ch == ''')
+    printf("'");
+  else
+  {
+    printf("please retry");
+  }
+}
