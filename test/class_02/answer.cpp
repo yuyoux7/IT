944aac7c0340f27770e988e06522a7f3dbd5eca5
yuyoux7 <91305304+yuyoux7@users.noreply.github.com>
2023-10-20 11:10:54 +0800

    Update answer.cpp

--- test/class_02/answer.cpp
+++ test/class_02/answer.cpp
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+int main()
+{
+  char ch;
+  printf("input \\ or ':");
+  scanf("%c", &ch);
+  if(ch == '\\')
+    printf("\\");
+  else if(ch == '\'')
+    printf("'");
+  else
+  {
+    printf("please retry\n");
+    main();
+  }
+}
