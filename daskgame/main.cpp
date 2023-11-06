93e8439dd433542e7e00b15ad671bdc930fbc094
yuyoux7 <91305304+yuyoux7@users.noreply.github.com>
2023-11-06 12:38:27 +0800

    Update

---
+++ daskgame/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+struct Data
+{
+    string name;
+    int lewgue;//理解
+    int seve;//存在
+    int inshin;//影響
+    int moden;//錨定
+    int see;//觀測
+    int san;//理智
+};
+int main()
+{
+    int number = 1;
+    Data player[5];
+    cin >> player[number].name;
+    cout << player[i].name;
+}
