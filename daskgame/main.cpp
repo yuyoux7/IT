e292963a8f1c08785be5f837da0954eb853c7cb8
yuyoux7 <91305304+yuyoux7@users.noreply.github.com>
2023-11-06 13:14:15 +0800

    Update main.cpp

--- daskgame/main.cpp
+++ daskgame/main.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 #define max_player 5
 using namespace std;
+int def()
+{
+    if(!=5.)
+    return (denfun monden - 100 );
+}
+int atk()
+{
+    return (inshin - seve);
+}
 struct Data
 {
     string name;//名稱
-    int lewgue;//理解
-    int seve;//存在
-    int inshin;//影響
-    int monden;//錨定
-    int see;//觀測
-    int san;//理智
+    int lewgue;//理解 6d*2
+    int seve;//存在 6d*2 atk_int=atk-def >0
+    int inshin;//影響 6d
+    int monden;//錨定 100
+    int see;//觀測 0
+    int san;//理智 100
+    int zundun;//種族 [1.人 see +5 2.神 lewgue +5 3.妖 inshin +5 4.污 define san 50 5.鬼 define monden 50]
 };
 int main()
 {
