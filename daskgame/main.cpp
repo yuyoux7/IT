1534d83a9d6441898ed94493f675c06998166530
yuyoux7 <91305304+yuyoux7@users.noreply.github.com>
2023-11-06 12:56:14 +0800

    Update main.cpp

--- daskgame/main.cpp
+++ daskgame/main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
+#define max_player 5
 using namespace std;
 struct Data
 {
-    string name;
+    string name;//名稱
     int lewgue;//理解
     int seve;//存在
     int inshin;//影響
-    int moden;//錨定
+    int monden;//錨定
     int see;//觀測
     int san;//理智
 };
 int main()
 {
     int number = 1;
-    Data player[5];
+    Data player[max_player];
     cin >> player[number].name;
     cout << player[number].name;
 }
